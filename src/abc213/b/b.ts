0deb38c31ffd68d9a36d29cd4fdb1e83978983cb
Shungo Nagamachi <shungo.nagamachi@vacancorp.com>
2022-06-16 15:33:05 +0900

    fix: dir

---
+++ src/abc213/b/b.ts
@@ -0,0 +1,10 @@
+const Main = (input) => {
+    const inputList = input.split("\n");
+    // const N = parseInt(inputList[0], 10);
+    const scoreList: number[] = inputList[1].split(" ").map(Number);
+
+    const score = [...scoreList].sort((a, b) => b - a)[1];
+    console.log(scoreList.indexOf(score) + 1);
+};
+
+Main(require("fs").readFileSync("/dev/stdin", "utbf8"));
---
+++ src/abc213/c/c.ts
@@ -0,0 +1,15 @@
+const Main = (input) => {
+    const inputList = input.split("\n");
+    const [H, W, N] = inputList[0].split(" ").map(Number);
+    console.log(H, W, N);
+
+    const cardList: number[][] = [];
+    for (let i = 0; i < N; i++) {
+        cardList.push(inputList[i + 1].split(" ").map(Number));
+    }
+
+    console.log(cardList);
+    // 重複除いた column, row の中での順位を出力すればよさそう
+};
+
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
